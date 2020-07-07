ab4fd994e954ea5eaf14def61026bfb331bbdd18
KissG549 <small_g@gmx.com>
2020-07-07 19:36:24 +0200

    Update TM1637DisplayTest.ino

--- HWTests/TM1637DisplayTest/TM1637DisplayTest.ino
+++ HWTests/TM1637DisplayTest/TM1637DisplayTest.ino
@@ -22,15 +22,45 @@ void setup() {
 void loop() {
 // set brightness
   display.print("8888");
-  //display.clear();
+  display.clear();
   display.print("1H");
   delay(1000);
-  for( uint8_t i = 0; i < 255; ++i )
+  display.clear();
+
+  uint8_t  my_command[5];
+
+  my_command[0] = B00000000;
+  my_command[1] = B00000000;
+  my_command[2] = B00000000;
+  my_command[3] = B00000000;
+  my_command[4] = B00000000;
+
+  for( uint8_t rounds = 0; rounds < 7; ++rounds )
   {
-    display.printNumber( i , false, true );
+     Serial.println(rounds);
+     
+     my_command[0] |= B00000001 << rounds;
+     my_command[1] |= B00000001 << rounds;
+     my_command[2] |= B00000001 << rounds;
+     my_command[3] |= B00000001 << rounds;
+     my_command[4] |= B00000001 << rounds;
+       
+    display.printRaw(my_command);
     
-    delay(500);
+    delay(100);
   }
   
-  delay(1000);
+  delay(2000);
+
+  my_command[0] = B00000000;
+  my_command[1] = B10000000;
+  my_command[2] = B00000000;
+  my_command[3] = B00000000;
+  my_command[4] = B00000000;
+
+  display.clear();
+
+  display.printRaw(my_command);
+
+  delay(2000);
 }
