52de86748e44d2e1b70fc707e498456b9700530e
KissG549 <small_g@gmx.com>
2020-07-26 19:53:42 +0200

    Update BasicCar.h

--- SmartCar/BasicCar.h
+++ SmartCar/BasicCar.h
@@ -4,11 +4,15 @@
 #include <AFMotor.h>
 #include <HCSR04.h>
 #include <inttypes.h>
+#include "Arduino.h"
+
+const double MIN_REQUIRED_DIST_FOR_TURN = 10.0;
 
 class BasicCar
 {
 public:
-	BasicCar(uint8_t pMotorFrontLeftPin,
+	BasicCar(
+		uint8_t pMotorFrontLeftPin,
 		uint8_t pMotorFrontRightPin,
 		uint8_t pMotorRearLeftPin,
 		uint8_t pMotorRearRightPin)
@@ -27,27 +31,33 @@ public:
 			DC 5V -> 160R/min
 			DC 6V -> 175R/min
 	*/
-		mMotorFrontLeft.setSpeed(255);
-		mMotorFrontRight.setSpeed(255);
-		mMotorRearLeft.setSpeed(255);
-		mMotorRearRight.setSpeed(255);
+		mMotorFrontLeft.setSpeed(240);
+		mMotorFrontRight.setSpeed(240);
+		mMotorRearLeft.setSpeed(240);
+		mMotorRearRight.setSpeed(240);
+		mDistanceSensor = nullptr;
 	};
 
 	~BasicCar() {};
 
+	void move();
 	void moveForward();
 	void moveBackward();
 	void turnLeft();
 	void turnRight();
 	void stop();
 
-	void avoidObstackle(UltraSonicDistanceSensor& pDistanceSensor);
+	void setDistanceSensor(UltraSonicDistanceSensor& pDistanceSensor);
+
+	void lookForDirection();
 
 private:
 	AF_DCMotor mMotorFrontLeft;
 	AF_DCMotor mMotorFrontRight;
 	AF_DCMotor mMotorRearLeft;
 	AF_DCMotor mMotorRearRight;
+
+	UltraSonicDistanceSensor *mDistanceSensor;
 
 	void motorsControl(const uint8_t pFrlCmd, uint8_t pFrrCmd = 0, uint8_t pRelCmd = 0, uint8_t pRerCmd = 0);
 };
