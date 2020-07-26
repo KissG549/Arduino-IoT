24776e1a9a43e47c989ee524ff9a4d6fb925cfc9
KissG549 <small_g@gmx.com>
2020-07-26 19:53:46 +0200

    Update SmartCar.ino

--- SmartCar/SmartCar.ino
+++ SmartCar/SmartCar.ino
@@ -1,41 +1,21 @@
-#include <AFMotor.h>
 #include <HCSR04.h>
 #include "BasicCar.h"
 
-UltraSonicDistanceSensor distanceSensor(2, 3); // Trigger pin, echo pin
-
 BasicCar myCar(4,3,1,2);
 
-void setup() {
+UltraSonicDistanceSensor distanceSensor(2, 3); // Trigger pin, echo pin
+
+void setup() 
+{
   Serial.begin(9600);
 }
 
-void loop() {
-
+void loop() 
+{
   double distance = distanceSensor.measureDistanceCm();
-  Serial.println(distance);
-  
-  myCar.moveForward();
-  Serial.println("Move forward");
-
-  delay(2000);
-
-  myCar.moveBackward();  
-  Serial.println("Move backward");
-
-  delay(2000);
-
-  myCar.turnLeft();
-  Serial.println("TURN LEFT");
-
-  delay(2000);
-
-  myCar.turnRight();
-  
-  Serial.println("TURN RIGHT");
-  
-  delay(2000);
-  myCar.stop();
-  
-  delay(2000);
+  myCar.setDistanceSensor(distanceSensor);
+  Serial.print("0 Distance: ");
+  Serial.println( distance );
+  myCar.move();
+  delay(100);
 }
