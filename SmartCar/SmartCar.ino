b7f6003696801c53f557bcc98df0eb6dde130633
KissG549 <small_g@gmx.com>
2020-08-24 11:17:55 +0200

    Comment removed

--- SmartCar/SmartCar.ino
+++ SmartCar/SmartCar.ino
@@ -12,10 +12,8 @@ const uint8_t TM1637_DATA_PIN = A0;
 
 SevenSegmentExtended    display(TM1637_CLK_PIN, TM1637_DATA_PIN);
 
-
 BasicCar myCar(4,3,1,2);
 
-//UltraSonicDistanceSensor distanceSensor(10, 9); // Trigger pin, echo pin
 DistanceManager distanceMNGR(10,9);
 
 void setup() 
