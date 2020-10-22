e622e0489cc119791c0c2528ba738d8e2709c289
KissG549 <57135398+KissG549@users.noreply.github.com>
2020-10-22 15:29:34 +0200

    Added log and reformat file

--- SmartCar/SmartCar.ino
+++ SmartCar/SmartCar.ino
@@ -1,3 +1,8 @@
+/*
+  Log: https://github.com/thijse/Arduino-Log/
+*/
+
+#include <ArduinoLog.h>
 #include <HCSR04.h>
 #include <SevenSegmentTM1637.h>
 #include <SevenSegmentExtended.h>
@@ -5,17 +10,17 @@
 #include "BasicCar.h"
 #include "DiMNGR.h"
 
+/* 
 #ifndef DEBUG_CAR
 #define DEBUG_CAR 1
 #endif
-
+*/
 const uint8_t DISPLAY_BRIGHTNESS = 30;
 
 const uint8_t TM1637_CLK_PIN  = A1;
 const uint8_t TM1637_DATA_PIN = A0;
 
-SevenSegmentExtended    display(TM1637_CLK_PIN, TM1637_DATA_PIN);
-
+SevenSegmentExtended    display(TM1637_CLK_PIN, TM1637_DATA_PIN); 
 BasicCar myCar(4,3,1,2);
 
 DistanceManager distanceMNGR(10,9);
@@ -28,6 +33,8 @@ void setup()
   display.print("INIT");
   myCar.setDisplay(&display);
   myCar.setDistanceMNGR(&distanceMNGR);
+
+  Log.begin(LOG_LEVEL_VERBOSE, &Serial);
 }
 
 void loop() 
